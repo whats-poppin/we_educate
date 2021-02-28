2e7a151fd38b3745170fd25e1547c6c2a5426d8c
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-01 00:05:01 +0530

    - testing actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>We Educate</h1>
+            <h1>We Educate Hosted Successfully</h1>
         </div>
     );
 }
