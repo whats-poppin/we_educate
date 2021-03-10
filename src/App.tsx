fd8cbf93cef970e26da1889b4c1f06616bd3e740
Mohan <mohanu526@gmail.com>
2021-03-10 13:58:01 +0530

    added CRUD syntax to refer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,66 @@
 import React from 'react';
 import './App.css';
+import {firestore} from "./firebase";
+
+//Add Data
+
+firestore.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+//Read Data
+
+firestore.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+
+// Create our initial doc
+firestore.collection("users").doc("frank").set({
+    name: "Frank",
+    favorites: {
+        food: "Pizza",
+        color: "Blue",
+        subject: "Recess"
+    },
+    age: 12
+}).then(function() {
+    console.log("Frank created");
+});
+
+// Update the doc without using dot notation.
+// Notice the map value for favorites.
+firestore.collection("users").doc("frank").update({
+    favorites: {
+        food: "Ice Cream"
+    }
+}).then(function() {
+    console.log("Frank food updated");
+});
+
+/*
+Ending State, favorite.color and favorite.subject are no longer present:
+/users
+    /frank
+        {
+            name: "Frank",
+            favorites: {
+                food: "Ice Cream",
+            },
+            age: 12
+        }
+ */
+
+
 
 function App() {
     return (
