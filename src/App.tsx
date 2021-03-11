a1000d4cbf2be45628c68f52ee50fc2ac3205401
antrikshmalik <malikantriksh@gmail.com>
2021-03-11 21:29:52 +0530

    Frontend boiler plate setup error removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,57 @@
 import React from 'react';
 import './App.css';
-import {firestore} from "./firebase";
-
+// import {firestore} from "./firebase";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    // Link
+} from "react-router-dom";
+import Home from "./Pages";
 //Add Data
 
-firestore.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    });
-
-//Read Data
-
-firestore.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
+// firestore.collection("users").add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+// })
+//     .then((docRef) => {
+//         console.log("Document written with ID: ", docRef.id);
+//     })
+//     .catch((error) => {
+//         console.error("Error adding document: ", error);
+//     });
+//
+// //Read Data
+//
+// firestore.collection("users").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//     });
+// });
 
 // Create our initial doc
-firestore.collection("users").doc("frank").set({
-    name: "Frank",
-    favorites: {
-        food: "Pizza",
-        color: "Blue",
-        subject: "Recess"
-    },
-    age: 12
-}).then(function() {
-    console.log("Frank created");
-});
+// firestore.collection("users").doc("frank").set({
+//     name: "Frank",
+//     favorites: {
+//         food: "Pizza",
+//         color: "Blue",
+//         subject: "Recess"
+//     },
+//     age: 12
+// }).then(function() {
+//     console.log("Frank created");
+// });
 
 // Update the doc without using dot notation.
 // Notice the map value for favorites.
-firestore.collection("users").doc("frank").update({
-    favorites: {
-        food: "Ice Cream"
-    }
-}).then(function() {
-    console.log("Frank food updated");
-});
+// firestore.collection("users").doc("frank").update({
+//     favorites: {
+//         food: "Ice Cream"
+//     }
+// }).then(function() {
+//     console.log("Frank food updated");
+// });
 
 /*
 Ending State, favorite.color and favorite.subject are no longer present:
@@ -64,9 +70,11 @@ Ending State, favorite.color and favorite.subject are no longer present:
 
 function App() {
     return (
-        <div className="App">
-            <h1>We Educate Hosted Successfully</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/Pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// @ts-ignore
+// import Navbar from "../Components/Navbar/navbar";
+
+const Home = () => {
+    return(
+        <>
+            {/*<Navbar/>*/}
+            <h1 style={{textAlign: "center", fontSize: "2.7rem"}}>WE EDUCATE HOSTED SUCCESSFULLY. DEV WORK STARTED</h1>
+        </>
+    )
+}
+
+export default Home;
