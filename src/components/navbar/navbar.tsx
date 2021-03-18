5543460f827fb8e3d80f80deb25dd1f9467760c4
MeanMachiNe13 <e19cse107@bennett.edu.in>
2021-03-18 16:17:15 +0530

    Stylistic changes to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,8 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './navbar.css';
 import {BsSearch} from "react-icons/bs";
 import {useHistory} from 'react-router-dom';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import {theme} from "../../utils/theme";
 
 export const NavB = () => {
+    const notMedium = useMediaQuery(theme.breakpoints.up('md'));
     const [showSearchBar, setShowSearchBar] = useState(false)
     const history = useHistory()
     const [selectedTab, setSelectedTab] = useState('')
@@ -19,41 +22,66 @@ export const NavB = () => {
 
     console.log(selectedTab);
 
-    return (
-        <>
-            <Navbar bg="light" expand="lg" id="mainNavbar" fixed="top">
-                <Navbar.Brand onClick={() => {
-                    history.push('/')
+    return !notMedium ? <><Navbar bg="light" expand="lg" id="mainNavbar" fixed="top">
+        <Navbar.Brand onClick={() => {
+            history.push('/')
+        }}>
+            <img src={brand_logo} alt="logo" className="brand_logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            {tabs.map((tab) =>
+                <Nav.Link onClick={() => {
+                    history.push(`/${tab}`);
                 }}>
-                    <img src={brand_logo} alt="logo" className="brand_logo"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    </Nav>
-                    <Form inline>
-                        <BsSearch className="search_button" onClick={() => {
-                            setShowSearchBar(!showSearchBar)
-                        }}/>
-                        {showSearchBar ? <div className="fade-in">
-                            <InputGroup>
-                                <FormControl
-                                    placeholder="Press Enter to search"
-                                    aria-label="Recipient's username"
-                                    aria-describedby="basic-addon2"
-                                />
-                            </InputGroup>
-                        </div> : <div/>}
-                    </Form>
-                    {tabs.map((tab) =>
-                        <Nav.Link onClick={() => {
-                            history.push(`/${tab}`);
-                        }}>
-                            {tab.toUpperCase().replaceAll('-', ' ')}
-                        </Nav.Link>
-                    )}
-                </Navbar.Collapse>
-            </Navbar>
-        </>
-    )
+                    {tab.toUpperCase().replaceAll('-', ' ')}
+                </Nav.Link>
+            )}
+            <Form inline>
+                <InputGroup style = {{width: "100%", margin : "0 0.2rem"}}>
+                    <FormControl
+                        placeholder="Search"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                </InputGroup>
+            </Form>
+        </Navbar.Collapse>
+    </Navbar></> : <>
+        <Navbar bg="light" expand="lg" id="mainNavbar" fixed="top">
+            <Navbar.Brand onClick={() => {
+                history.push('/')
+            }}>
+                <img src={brand_logo} alt="logo" className="brand_logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                </Nav>
+                <Form inline>
+                    <BsSearch className="search_button" onClick={() => {
+                        setShowSearchBar(!showSearchBar)
+                    }}/>
+                    {showSearchBar ? <div className="fade-in">
+                        <InputGroup>
+                            <FormControl
+                                placeholder="Press Enter to search"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+                    </div> : <div/>}
+                </Form>
+                {tabs.map((tab) =>
+                    <Nav.Link onClick={() => {
+                        history.push(`/${tab}`);
+                    }}>
+                        {tab.toUpperCase().replaceAll('-', ' ')}
+                    </Nav.Link>
+                )}
+            </Navbar.Collapse>
+        </Navbar>
+    </>
 };
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <>
             <NavB/>
-            <ViewProfile/>
+            {/*<ViewProfile/>*/}
         </>
     )
 };
