ff442ae7f018fea8a941d5e1e0ca770b10fdbd5c
MeanMachiNe13 <e19cse107@bennett.edu.in>
2021-03-18 19:51:03 +0530

    Added boiler plate for edit profile and fixed placeholder bug on Mozilla

---
+++ src/components/edit-profile/editProfile.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './editProfile.css'
+import {Button, Col, Form, Nav, Row} from "react-bootstrap";
+import SideNav from "../side-nav/sideNav";
+
+const EditProfile = () => {
+    return (
+        <div className = "rootContainer">
+            <SideNav/>
+            <div className="info">
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Group controlId="formGridAddress1">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control placeholder="1234 Main St" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor" />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control as="select" defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Group id="formGridCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default EditProfile;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {ViewProfile} from '../components/view-profile/viewProfile'
 import {NavB} from "../components/navbar/navbar";
+import EditProfile from "../components/edit-profile/editProfile";
 
 const Profile = () => {
     return (
         <>
             <NavB/>
             {/*<ViewProfile/>*/}
+            <EditProfile/>
         </>
     )
 };
