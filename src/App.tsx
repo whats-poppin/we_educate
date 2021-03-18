76f9ab6f76b405e2ffbe064a9ca5be907af26a2d
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-19 01:36:51 +0530

    - minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,12 @@ import {ThemeProvider} from "@material-ui/styles";
 import {theme} from "./utils/theme";
 import {Explore} from "./pages/explore";
 import {Course} from "./pages/course";
-import {Auth} from "./pages/auth";
 import {AuthProvider} from "./contexts/auth";
 import {PrivateRoutes} from "./components/private-routes/private-routes";
 import {ErrorBoundary} from "./utils/error-boundary";
 import {SnackbarToggleProvider} from './contexts/snackbar-toggle';
-//Add Data
-
-// firestore.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// })
-//     .then((docRef) => {
-//         console.log("Document written with ID: ", docRef.id);
-//     })
-//     .catch((error) => {
-//         console.error("Error adding document: ", error);
-//     });
-//
-// //Read Data
-//
-// firestore.collection("users").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log(`${doc.id} => ${doc.data()}`);
-//     });
-// });
-
-// Create our initial doc
-// firestore.collection("users").doc("frank").set({
-//     name: "Frank",
-//     favorites: {
-//         food: "Pizza",
-//         color: "Blue",
-//         subject: "Recess"
-//     },
-//     age: 12
-// }).then(function() {
-//     console.log("Frank created");
-// });
 
 // Update the doc without using dot notation.
-// Notice the map value for favorites.
 // firestore.collection("users").doc("frank").update({
 //     favorites: {
 //         food: "Ice Cream"
@@ -55,19 +19,6 @@ import {SnackbarToggleProvider} from './contexts/snackbar-toggle';
 // }).then(function() {
 //     console.log("Frank food updated");
 // });
-
-/*
-Ending State, favorite.color and favorite.subject are no longer present:
-/users
-    /frank
-        {
-            name: "Frank",
-            favorites: {
-                food: "Ice Cream",
-            },
-            age: 12
-        }
- */
 
 
 const App = () => {
@@ -78,7 +29,6 @@ const App = () => {
                     <Router>
                         <Switch>
                             <Route path="/" component={Home} exact/>
-                            <Route path="/auth" component={Auth} exact/>
                             <Route path="/explore" component={Explore} exact/>
                             <Route path={"/course"} component={Course} exact/>
                             <PrivateRoutes/>
