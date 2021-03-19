1b8707ad41685a3278e6334c2ad67b9e7b1b87cb
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-19 17:26:32 +0530

    - fixed auth-context bug

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,16 +1,28 @@
 import React, {useEffect, useState} from "react";
 import {auth} from "../firebase";
+import {Individual} from "../models/individual";
+import firebase from 'firebase';
+import {getLoggedInUser} from "../controllers/auth-controller";
 
 export const AuthContext = React.createContext(null);
 
 export const AuthProvider: React.FC = ({children}) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<Individual | undefined>(null);
     useEffect(() => {
         (async () => {
-            auth.onAuthStateChanged(async (userAuth: any) => {
-                setUser(userAuth);
+            auth.onAuthStateChanged(async (userAuth: firebase.User) => {
+                if (userAuth) {
+                    const user = await getLoggedInUser(userAuth.uid);
+                    if (typeof user !== 'string')
+                        setUser(user as Individual);
+                    else
+                        setUser(null);
+                } else {
+                    setUser(null);
+                }
+
             });
-        })()
+        })();
     }, []);
 
     return (
