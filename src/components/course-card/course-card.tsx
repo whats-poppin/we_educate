c9f2cb150ee5b335914c40e0a245547e24bfb47d
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-20 02:44:47 +0530

    - added course card barebones

---
+++ src/components/course-card/course-card.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Card} from "@material-ui/core";
+export const CourseCard = () => {
+    return <Card>
+        Card init
+    </Card>;
+};
--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {CourseCard} from "../components/course-card/course-card";
 
 export const Explore = () => {
     return <>
         <h1 style={{marginTop: '7rem'}}>
             Explore
         </h1>
+        <CourseCard/>
     </>;
 };
