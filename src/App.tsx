098b2c0f05fa7095290875b400ce03e6db93ea29
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-23 17:17:09 +0530

    - fixed minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/home";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./utils/theme";
-import { Explore } from "./pages/explore";
-import { Course } from "./pages/course";
 import { AuthProvider } from "./contexts/auth";
-import { PrivateRoutes } from "./components/private-routes/private-routes";
+import { Routes } from "./components/private-routes/routes";
 import { ErrorBoundary } from "./utils/error-boundary";
 import { SnackbarToggleProvider } from './contexts/snackbar-toggle';
 import { NavB } from "./components/navbar/navbar";
 import { UserDetailsProvider } from "./contexts/user-details";
-import { TnC } from "./pages/tnc";
-import { About } from "./pages/about";
 
 // Update the doc without using dot notation.
 // firestore.collection("users").doc("frank").update({
@@ -34,12 +29,7 @@ const App = () => {
                         <Router>
                             <NavB/>
                             <Switch>
-                                <Route path="/" component={ Home } exact/>
-                                <Route path="/explore" component={ Explore } exact/>
-                                <Route path={ "/course" } component={ Course } exact/>
-                                <Route path={ "/about" } component={ About } exact/>
-                                <Route path={ "/t&c" } component={ TnC } exact/>
-                                <PrivateRoutes/>
+                                <Routes/>
                             </Switch>
                         </Router>
                     </UserDetailsProvider>
