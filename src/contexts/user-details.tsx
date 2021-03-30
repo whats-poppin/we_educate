797733d69368c0b6e8c6c986cdc659556c1bcb75
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-03-31 00:47:55 +0530

    - fixed user-context issue

--- src/contexts/user-details.tsx
+++ src/contexts/user-details.tsx
@@ -1,10 +1,23 @@
-import React, {useState} from "react";
-import {Individual} from "../models/individual";
+import React, { useContext, useEffect, useState } from "react";
+import { Individual } from "../models/individual";
+import { AuthContext } from "./auth";
+import firebase from 'firebase';
+import { getLoggedInUser } from "../controllers/auth-controller";
 
 export const UserDetailsContext = React.createContext(null);
 
-export const UserDetailsProvider: React.FC = ({children}) => {
-    const [user, setUser] = useState<Individual | undefined>(null);
+export const UserDetailsProvider: React.FC = ({ children }) => {
+    const [ user, setUser ] = useState<Individual | undefined>(null);
+    const { authStatus }: { authStatus: firebase.User } = useContext(AuthContext);
 
-    return <UserDetailsContext.Provider value={{user, setUser}}>{children}</UserDetailsContext.Provider>
+    useEffect(() => {
+        if ( authStatus ) {
+            ( async () => {
+                const user = await getLoggedInUser(authStatus.uid);
+                setUser(user as Individual)
+            } )()
+        }
+    }, [ authStatus ]);
+
+    return <UserDetailsContext.Provider value={ { user, setUser } }>{ children }</UserDetailsContext.Provider>
 };
