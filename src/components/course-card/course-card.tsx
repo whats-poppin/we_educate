808fe20c1797b4503ed5d999a1a42924a3645cce
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-04-02 18:09:47 +0530

    - added card-flip bare-bones

--- src/components/course-card/course-card.tsx
+++ src/components/course-card/course-card.tsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
-import { Card } from "@material-ui/core";
+import React, { useContext, useState } from 'react';
 import { Product } from "../../models/product";
 import { UserDetailsContext } from "../../contexts/user-details";
 import { useHistory } from 'react-router-dom';
+import ReactCardFlip from "react-card-flip";
+import { Button } from '@material-ui/core';
 
 export const CourseCard: React.FC<{ course: Product }> = React.memo(({ course }) => {
     const { user } = useContext(UserDetailsContext);
     const history = useHistory();
+    const [ isFlipped, setIsFlipped ] = useState(false);
 
     const ownedCourse: boolean = ( () => user?.product.includes(course.id) )();
 
-    return <Card>
-        { course.name }
-        <br/>
-        <h3 onClick={ () => {
-            history.push(`/course?id=${ course.id }`)
-        } }>
-            { ownedCourse ? 'Go to course' : 'know more' }
-        </h3>
-    </Card>;
+    return <ReactCardFlip isFlipped={ isFlipped }>
+        <>
+            { course.name }
+            <br/>
+            <Button onClick={ () => setIsFlipped(!isFlipped) }>
+                Flip
+            </Button>
+        </>
+        <>
+            <Button onClick={ () => setIsFlipped(!isFlipped) }>
+                Flip
+            </Button>
+            <h3 onClick={ () => {
+                history.push(`/course?id=${ course.id }`)
+            } }>
+                { ownedCourse ? 'Go to course' : 'know more' }
+            </h3>
+        </>
+    </ReactCardFlip>;
 });
