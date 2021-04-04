6330e8f0d0235a4a0d9f742a2617657f93195a4a
MeanMachiNe13 <e19cse107@bennett.edu.in>
2021-04-04 23:39:48 +0530

    -changed homepage background and layout

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -1,66 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from '../../assets/brand_logo.png'
-import {Button, FormControl, InputGroup} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 import './Jumbotron.css';
-import {AiOutlineArrowRight} from "react-icons/ai";
 import {MdKeyboardArrowDown} from "react-icons/md";
-import {useHistory} from 'react-router-dom';
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const Jumbotron = () => {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
     const notSmall = useMediaQuery('(min-width:500px)');
-    const validateEmail = () => {
-        const atPos = email.indexOf("@");
-        const dotPos = email.lastIndexOf(".");
-        return (atPos < 1 || (dotPos - atPos < 2));
-    }
     return (
         <>
             <div className="container-grid">
-                <div className="item">
-                    <h1>Committed to Excellence</h1>
-                    <p style={{fontWeight: 600}}>
-                        What is the purpose of Education?
-                        Enlightenment.
-                        <br/>
-                        What is the purpose of Management Education?
-                        <br/>
-                        Transformation - to make individuals think differently.
-                    </p>
-                </div>
+                {/*<div className="item">*/}
+                {/*    <h1>Committed to Excellence</h1>*/}
+                {/*    <p style={{fontWeight: 600}}>*/}
+                {/*        What is the purpose of Education?*/}
+                {/*        Enlightenment.*/}
+                {/*        <br/>*/}
+                {/*        What is the purpose of Management Education?*/}
+                {/*        <br/>*/}
+                {/*        Transformation - to make individuals think differently.*/}
+                {/*    </p>*/}
+                {/*</div>*/}
                 <img src={logo} alt="logo" id="logo" />
             </div>
             <div className="container-flex"
                  style={notSmall ? {justifyContent: 'center'} : {justifyContent: 'flex-start', padding: '0rem 1rem'}}>
-                <form>
-                    <InputGroup>
-                        <FormControl
-                            placeholder="Enter your e-mail"
-                            value={email}
-                            onChange={(e) =>
-                                setEmail(e.target.value)}
-                            aria-label="Recipient's username"
-                            aria-describedby="basic-addon2"
-                        />
-                        <InputGroup.Append>
-                            <Button type="submit"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        history.push({
-                                            pathname: `/auth`,
-                                            state: {email, type: 'signup'}
-                                        });
-                                    }}
-                                    disabled={validateEmail()}
-                                    variant="outline-secondary">
-                                <AiOutlineArrowRight/>
-                            </Button>
-                        </InputGroup.Append>
-                    </InputGroup>
-                </form>
                 <div className="arrow-link">
                     <div className="arrows">
                         <MdKeyboardArrowDown className="arrow"/>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import Jumbotron from "../components/jumbotron/Jumbotron";
 import Footer from "../components/footer/footer";
 import WeCarousel from "../components/carousel/we-carousel"
-import CourseFlipCard from "../components/course-flip-card/course-flip-card";
 
 const Home = () => {
     return (
         <>
-            <Jumbotron/>
-            <WeCarousel/>
-            <CourseFlipCard/>
+            <div className="home-body">
+                <Jumbotron/>
+                <WeCarousel/>
+            </div>
             <Footer/>
-
         </>
     )
 }
