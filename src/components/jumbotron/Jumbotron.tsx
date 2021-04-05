c741c78d5330d7753543a09e986bb30642457138
MeanMachiNe13 <e19cse107@bennett.edu.in>
2021-04-05 08:57:54 +0530

    added animated text in hero section

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from '../../assets/brand_logo.png'
 import 'bootstrap/dist/css/bootstrap.css';
 import './Jumbotron.css';
 import {MdKeyboardArrowDown} from "react-icons/md";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+// @ts-ignore
+import Typist from 'react-typist';
 
 const Jumbotron = () => {
+    const [count, setCount] = useState(1);
+    useEffect(() => {
+        // document.title = `You clicked ${count} times`;
+        // console.log("Count: " + count);
+        setCount(1);
+    }, [count]);
     const notSmall = useMediaQuery('(min-width:500px)');
     return (
         <>
@@ -22,6 +30,23 @@ const Jumbotron = () => {
                 {/*    </p>*/}
                 {/*</div>*/}
                 <img src={logo} alt="logo" id="logo" />
+                <p style={{marginTop: '2rem',
+                    fontFamily: 'Montserrat, sans-serif',
+                    fontSize: '2rem',
+                }}>
+                    {count ? (
+                        <Typist avgTypingDelay={50} onTypingDone={() => setCount(0)}>
+                            <span> We Educate</span>
+                            <Typist.Backspace count={10} delay={800} />
+                            <span> Committed to Excellence</span>
+                            <Typist.Backspace count={23} delay={800} />
+                            {/*<span> second text that replaces is here</span>*/}
+                        </Typist>
+                    ) : (
+                        ""
+                    )}
+                </p>
+
             </div>
             <div className="container-flex"
                  style={notSmall ? {justifyContent: 'center'} : {justifyContent: 'flex-start', padding: '0rem 1rem'}}>
