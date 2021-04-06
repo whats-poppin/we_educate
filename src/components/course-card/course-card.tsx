42a0ab64e21d3be8f6f57af160b75544943a37bd
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-04-07 01:55:44 +0530

    - updated card styling

--- src/components/course-card/course-card.tsx
+++ src/components/course-card/course-card.tsx
@@ -3,7 +3,7 @@ import { Product } from "../../models/product";
 import { UserDetailsContext } from "../../contexts/user-details";
 import { useHistory } from 'react-router-dom';
 import ReactCardFlip from "react-card-flip";
-import { Button,Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import "./course-card.css";
 
 export const CourseCard: React.FC<{ course: Product }> = React.memo(({ course }) => {
@@ -12,37 +12,57 @@ export const CourseCard: React.FC<{ course: Product }> = React.memo(({ course })
     const [ isFlipped, setIsFlipped ] = useState(false);
 
     const ownedCourse: boolean = ( () => user?.product.includes(course.id) )();
-
     return <ReactCardFlip isFlipped={ isFlipped }>
         <div>
-            <Card style = {{height: '28rem', width: '22rem'}}
+            <Card style={ { height: '28rem', width: '22rem' } }
                   className="course"
                   onClick={ () => setIsFlipped(!isFlipped) }
             >
-                <Card.Img variant="top" src={course.imgUrl} />
+                <Card.Img variant="top" src={ course.imgUrl }/>
                 <Card.Body>
-                    <Card.Title style = {{fontWeight: 'bolder', padding: '0.8rem 0 1rem 0', fontSize: '1.14rem'}}>{ course.name  }</Card.Title>
-                    <Card.Text style={{fontSize: '1.2rem', fontFamily: 'Lato, sans-serif'}}>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                    <Card.Title style={ {
+                        fontWeight: 'bolder',
+                        padding: '0.8rem 0 1rem 0',
+                        fontSize: '1.14rem'
+                    } }>{ course.name }</Card.Title>
+                    <Card.Text style={ { fontSize: '1rem', fontFamily: 'Lato, sans-serif' } }>
+                        Meant for <span style={ { fontWeight: 'bold' } }>{ course.meta.participantLevel }</span>
+                        <br/>
+                        Duration <span style={ { fontWeight: 'bold' } }>{ course.meta.duration }</span>
+                        <br/>
+                        Taught By <span style={ { fontWeight: 'bold' } }>{ course.meta.faculty }</span>
+                    </Card.Text>
+                    <Card.Text style={ {
+                        fontWeight: 'bold', fontSize: '1.6rem', position: "absolute",
+                        right: '20px',
+                    } }> { '>' }
                     </Card.Text>
                 </Card.Body>
             </Card>
         </div>
         <div>
-            <Card style = {{height: '28rem', width: '22rem' }}
+            <Card style={ { height: '28rem', width: '22rem' } }
                   className="course"
                   onClick={ () => setIsFlipped(!isFlipped) }
             >
-                <Card.Img variant="top" src={course.imgUrl} />
+                <Card.Img variant="top" src={ course.imgUrl }/>
                 <Card.Body>
-                    <Card.Title style = {{fontWeight: 'bolder', padding: '0.8rem 0 1rem 0', fontSize: '1.14rem'}}>{ course.name }</Card.Title>
-                    <Card.Text >
+                    <Card.Title style={ {
+                        fontWeight: 'bolder',
+                        padding: '0.8rem 0 0.8rem 0',
+                        fontSize: '1.14rem'
+                    } }>{ course.name }</Card.Title>
+                    <Card.Text style={ { fontSize: '1.2rem', fontFamily: 'Lato, sans-serif', textAlign: 'left' } }>
+                        { course.meta?.competenciesDevelopment.slice(0, course.name === 'MANAGING CORPORATE REPUTATION' ? 1 : 2).map((e, idx) => <>
+                            { idx + 1 }. { e }
+                            <br/>
+                        </>) }
+                        <span style={ { fontWeight: 'bold' } }>And more ....</span>
                         <div className="card-button">
                             <Button variant="dark"
-                                onClick={ () => {
-                                history.push(`/course?id=${ course.id }`)
-                            }} >
+                                    onClick={ () => {
+                                        history.push(`/course?id=${ course.id }`)
+                                    } }>
                                 { ownedCourse ? 'Go to course' : 'KNOW MORE' }
                             </Button>
                         </div>
