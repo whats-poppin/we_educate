38ebf1a7efc425fea4a929c55e41617c04157f8a
MeanMachiNe13 <e19cse107@bennett.edu.in>
2021-05-15 12:59:27 +0530

    course page responsiveness updated

--- src/pages/tnc.tsx
+++ src/pages/tnc.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import CoursePage from "../components/course-page/course-page";
+import Footer from "../components/footer/footer";
 
 export const TnC = () => {
-    return <h1 style={ { margin: '6rem' } }> T&C</h1>
+    return (
+        <>
+            {/*<h1 style={ { margin: '6rem' } }> T&C</h1>*/}
+            <CoursePage/>
+            <Footer/>
+        </>
+    )
 };
